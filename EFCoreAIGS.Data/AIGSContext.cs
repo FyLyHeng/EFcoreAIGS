89c209c7f710a73184ef21542cfda01a4053ab53
fylyheng.phally <fylyheng.phally@gmail.com>
2022-06-03 00:55:58 +0700

    Complete AppConfiguration

--- EFCoreAIGS.Data/AIGSContext.cs
+++ EFCoreAIGS.Data/AIGSContext.cs
@@ -1,7 +1,7 @@
-﻿using EFCoreAIGS.Data.Configuration.EntityConfig;
+﻿using EFCoreAIGS.Data.Configuration.AppConfig;
+using EFCoreAIGS.Data.Configuration.EntityConfig;
 using EFCoreAIGS.Data.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace EFCoreAIGS.Data
@@ -10,7 +10,6 @@ namespace EFCoreAIGS.Data
     {
         public AIGSContext()
         {
-            // for datetime from C# to postgres we need to configure this below because it different format 
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
             //AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
         }
@@ -22,20 +21,8 @@ namespace EFCoreAIGS.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
-            string solutionPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName);
-            string projectNameSpace = GetType().Namespace;
-            string fileConfigName = "appsettings.json";
-
-
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile($"{solutionPath}/{projectNameSpace}/{fileConfigName}",optional:true)
-                .Build();
-            
             optionsBuilder
-                .UseNpgsql(@$"{configuration.GetConnectionString("url")}")
-                // .UseNpgsql(@"Host=localhost;Username=postgres;Password=P@ssw0rd;Database=EFCore")
+                .UseNpgsql(@$"{AppConfiguration.ConnectionString}")
                 .LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name}, LogLevel.Information)
                 .EnableSensitiveDataLogging();
         }
--- EFCoreAIGS.Data/Configuration/AppConfig/AppConfiguration.cs
+++ EFCoreAIGS.Data/Configuration/AppConfig/AppConfiguration.cs
@@ -4,32 +4,25 @@ namespace EFCoreAIGS.Data.Configuration.AppConfig
 {
     public class AppConfiguration
     {
-        public AppConfiguration()
+        public readonly static string ConnectionString;
+
+        static AppConfiguration()
         {
-            var configurationBuilder = new ConfigurationBuilder();
-            Console.WriteLine($"Root : {Directory.GetCurrentDirectory()}");
-            
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
-            configurationBuilder.AddJsonFile(path, false);
-            
-            
-            Console.WriteLine($"Qpath: {Directory.GetDirectoryRoot(Directory.GetCurrentDirectory())}");
-            Console.WriteLine($"path: {path}");
+            Console.WriteLine("does it work or not");
+            string solutionPath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.Parent!.FullName);
+            string projectNameSpace = "EFCoreAIGS.Data";
+            string fileConfigName = "appsettings.json";
 
+            Console.WriteLine($"{projectNameSpace}");
 
-            var root = configurationBuilder.Build();
-            ConnectionString = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+            Console.WriteLine($"{solutionPath}/{projectNameSpace}/{fileConfigName}");
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"{solutionPath}/{projectNameSpace}/{fileConfigName}",optional:true)
+                .Build();
+            
+            ConnectionString = configuration.GetConnectionString("url")!;
         }
-        
-        
-        // public AppConfiguration()
-        // {
-        //     var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false);
-        //
-        //     IConfiguration config = builder.Build();
-        //
-        //     ConnectionString = config.GetValue<string>("ConnectionStrings:DataConnection");
-        // }
-        public string? ConnectionString { get; }
     }
 }
